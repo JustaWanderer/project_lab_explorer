72c13ae8b80a99071ac736c9d6a3b39dc0c3f7f1
JustaWanderer <Wilczur.new@gmail.com>
2018-06-11 01:11:13 +0200

    general inits

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const http = require('http');
+// const http = require('http');
 const express = require('express');
 const app = express();
-const path = require("path")
+const path = require('path');
+const editorRouter = require('./modules/editor/editor-router');
 
 app.use(express.static('static'));
+app.use(editorRouter);
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('static/index.html'));
-});
-
-app.get("/editor", (req, res) => {
-    res.sendFile(path.resolve('static/editor.html'));
 });
 
 app.listen(3000, () => {
