a988c94b1dfd5c75b8265d11156060e49008b0e5
Coprobo <35069447+TheCoprobo@users.noreply.github.com>
2018-06-13 17:35:54 +0200

    imported fbx loader

--- static/js/ResLoader.js
+++ static/js/ResLoader.js
@@ -10,6 +10,7 @@ class ResLoader {
     constructor() {
         this.textureLoader = new THREE.TextureLoader();
         this.modelLoader = new THREE.JSONLoader();
+        this.FBXLoader = new THREE.FBXLoader();
     }
 
     /**
@@ -26,15 +27,28 @@ class ResLoader {
     }
 
     /**
-     * Loads a texture from a file
+     * Loads a geometry from a file
      * @param {string} path relative path to the resource
      * @return {Promise<THREE.Geometry | ErrorEvent>}
      * @since 1.0.0
      * @method
      */
-    loadTexture(path) {
+    loadGeometry(path) {
         return new Promise((resolve, reject) => {
             this.modelLoader.load(path, resolve, null, reject);
+        });
+    }
+
+    /**
+     * Loads a FBX Model from a file
+     * @param {string} path relative path to the resource
+     * @return {Promise<THREE.Geometry | ErrorEvent>}
+     * @since 1.0.0
+     * @method
+     */
+    loadFBX(path) {
+        return new Promise((resolve, reject) => {
+            this.FBXLoader.load(path, resolve, null, reject);
         });
     }
 }
